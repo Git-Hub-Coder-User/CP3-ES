add78d2a2015997cfeee53446dadd080910fbe84
Git-Hub-Coder-User <elsa.schultheiss@ucas-edu.net>
2024-10-14 17:48:53 +0000

    Created UML notes file and took notes

---
+++ Notes/UML_notes.py
@@ -0,0 +1,74 @@
+"""
+Don't make me throw things at you - LaRose
+You're not allowed to die in my classroom - LaRose
+It's a safety thing - LaRose
+Cyber secuirity people would get on my case if I didn't tell you that - LaRose
+Genetics don't apply - LaRose
+Our classes have strong genetics - LaRose
+Aggregation which gives me epilitic fits - LaRose
+Brain functions: Not present - LaRose
+You never thought you'd learn so much about finances in programming, did you - LaRose
+"""
+
+"""
+UML: Unified Modeling Langauge is used to model code and using graphical notation to represent the code and organization. It's a standardized notation for OOP. 
+
+    Structural Diagrams
+        Composite Structure Diagram 
+        Deployment diagram
+        Object Duagram
+        Profile Diagram
+          Class Diagram
+        Object Diagram 
+        Component Diagram. 
+
+    Behavioral Diagrams
+        Activity Diagram 
+        Use Case Diagram 
+        State Machine Diagram 
+        Interaction Diagram
+            Sequence Diagram 
+            Communication Diagram
+            Interaction Overview Diagram
+            Timing Diagram
+    
+    Class diagram
+        Describes the types of diagrams and their relationships
+            Assosiation, inheritance, etc
+    Component diagram
+        Architecture of software components and their relationships
+    Deployment diagram is a shows the software as a deployment block
+        Visualizes distribution of artificats
+    Object Diagram
+        Shows a system's state at a specific point in time
+    Package Diagram
+        Shows packages and tendencies
+            Simulate layered application
+    Composite Structure Diagram
+        used in micro level system modeling and shows individual items
+        Shows how stuff communicates
+        Set of interrelated elements
+    Profile diagram
+        Domain and platform specific somethings and their relationships
+    Use case diagrams
+        Model of inteneded functions
+    Activity digarmas
+        Grapihc representations of workflows
+    State Machine Diagram
+        Used to describe system layers
+        Life cycle of items
+    Communication Diagram
+        Used to show ohw stuff 'talks' to eachother
+    Interaction Overview Diagram
+        Overview of flow of control
+    Timing Diagram
+        Shows how stuff interacts during a specific period
+
+We're using class diagrams
+Larose uses Lucidchart
+
+Classes are a box
+    Top category has all of the attributes of the class, i.e. any variables we're setting
+    Second category has all the methods/operations
+    https://docs.google.com/presentation/d/1taGYUGDU_hjXW55DAZjSKRLyP1GZgQg6Kythu9Zlndc/edit#slide=id.p 
+"""
