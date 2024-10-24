0e40b7591e6671f10aeaac80644655202a700833
Git-Hub-Coder-User <elsa.schultheiss@ucas-edu.net>
2024-10-24 17:18:30 +0000

    Changed thing in Sundae

--- testing/dessert/test_dessert.py
+++ testing/dessert/test_dessert.py
@@ -42,7 +42,7 @@ def test_IceCream():
 def test_Sundae():
     item1 = Sundae("Vanilla", 3, 2.00, "Sprinkles", 1.00)
     item2 = Sundae("Chocolate", 4, 1.00, "Chocolate chips", 0.45)
-    item3 = Sundae("Strawberry", 1, 5.00, "Cherry", 3.00)
+    item3 = Sundae("Mint", 1, 2.49, "Cherry", 3.00)
     assert item1.name == "Vanilla"
     assert item2.scoop_count == 4
     assert item2.price_per_scoop == 1.00
