753426fb59d65eac4d29ba917ee7fd777436950d
Git-Hub-Coder-User <elsa.schultheiss@ucas-edu.net>
2024-10-31 19:32:38 +0000

    Added name

--- HalloweenClasses.py
+++ HalloweenClasses.py
@@ -3,54 +3,59 @@ class Monster:
         self.origin = None
         self.attack = None
         self.move = None
+        self.name = None
     
     def __str__(self):
         return(f"is from {self.origin} and currently has {self.attack} with {self.move}! ")
 
     def combat(self, other):
-        print("Our challenger", self)
-        print("Our defender", other)
+        print(f"Our challenger, {self.name},", self)
+        print(f"Our defender, {other.name},", other)
         if self.attack > other.attack:
             self.attack += 1
-            return "Victory! "
+            return f"{self.name} won! "
         elif self.attack < other.attack:
             other.attack += 1
-            return "Loss! "
+            return f"{other.name} won! "
         else:
             return "Tie"
         
 class Mummy(Monster):
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
         self.origin = "Egypt"
         self.attack = 15
         self.move = "Mummy Tangle"
+        self.name = name
 
 class Skeleton(Monster):
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
         self.origin = "The Void"
         self.attack = 5
         self.move = "Rattle Clattle"
+        self.name = name
 
 class Cobra(Monster):
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
         self.origin = "The Amazon"
         self.attack = 17
         self.move = "Hug of Doom"
+        self.name = name
 
 class Witch(Monster):
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
         self.origin = "Swamp"
         self.attack = 25
         self.move = "Dark Magic"
+        self.name = name
 
-Susan = Mummy()
-Jack = Skeleton()
-Serphant = Cobra()
-Hex = Witch()
+Susan = Mummy("Susan")
+Jack = Skeleton("Jack")
+Serphant = Cobra("Serphant")
+Hex = Witch("Hex")
 
 print("Susan", Susan)
 
